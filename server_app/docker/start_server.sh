fc4bef390b8951b804796054b3d386da71e118cc
Magyar Dávid <aoloyk@inf.elte.hu>
2023-03-19 21:40:15 +0100

    [server_app] Update start_server.sh

--- server_app/docker/start_server.sh
+++ server_app/docker/start_server.sh
@@ -3,6 +3,21 @@
 ARGS=$@
 PYTHON_ARGS=()
 
+if [[ -f "/mnt/config.docker.json" ]]; then
+  cp /mnt/config.docker.json /workspace/resources/config.local.json
+else
+  echo "No mounted config file!"
+  exit 1
+fi;
+
+if [[ -d "/mnt/output" ]]; then
+  mkdir -p /mnt/output/satellite_images/automatic
+  mkdir -p /mnt/output/webapp_results/automatic
+else
+  echo "No mounted output directory!"
+  exit 1
+fi;
+
 if echo "${ARGS[@]}" | grep -qw "startup"; then
   PYTHON_ARGS+=("-st")
 fi;
