f3e2f9b4422e0bbce4099b5c399b9044ba17f67e
Máté Cserép <mcserep@inf.elte.hu>
2023-03-25 12:47:52 +0100

    [web_app] unify web app into single build

---
+++ web_app/app/vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+
+const BASE_URL = process.env.VITE_BASE_URL || '';
+
+export default {
+  base: BASE_URL,
+  build: {
+    sourcemap: true,
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        demo: resolve(__dirname, 'demo.html'),
+      },
+    },
+  }
+}
--- web_app/demo/vite.config.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = process.env.VITE_BASE_URL || '';
-
-export default {
-  base: BASE_URL,
-  build: {
-    sourcemap: true,
-    target: 'esnext',
-  }
-}
