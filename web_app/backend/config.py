6d7ab254968da0577e85e923e55e2211f1f6102c
Magyar Dávid <aoloyk@inf.elte.hu>
2024-12-02 01:23:38 +0100

    [web_app_backend] #14 Add sanity check to env variables in config.py

--- web_app/backend/config.py
+++ web_app/backend/config.py
@@ -2,7 +2,17 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("PSQL_DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#    DATABASE_HOST = os.getenv("DATABASE_HOST", "host.docker.internal")
+
+    @staticmethod
+    def check_env_variables() -> None:
+        if Config.SECRET_KEY is None:
+            raise ValueError(
+                f"The 'FLASK_SECRET_KEY' must be supplied using the '-e' flag when starting the Docker container."
+            )
+        if Config.SQLALCHEMY_DATABASE_URI is None:
+            raise ValueError(
+                f"The 'PSQL_DATABASE_URL' must be supplied using the '-e' flag when starting the Docker container."
+            )
